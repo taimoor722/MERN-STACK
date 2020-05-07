a440ef900920d6cb5908349c44953f03fec0a3df
taimoor arif <taimoor722@gmail.com>
2020-05-07 19:39:01 +0500

    js

---
+++ javascript/app.js
@@ -0,0 +1,87 @@
+// alert("hello world");
+
+// var a = 5;
+// var b=10;
+// var c=a+b;
+// alert(c)
+
+// var a =5;
+// var b= 10;
+// var c = a+b;
+
+// console.log(c);
+
+
+// var  x= 5;
+// var y="ali";
+// var z= y+x;
+// alert(z);
+
+
+// var x="ghouse";
+// var y="ali";
+// var z = y+x;
+
+// alert(z);
+
+// var f =20;
+// var g="10";
+// var h = f+g;
+// alert(h);
+// 
+
+
+// var f =20;
+// var g="2";
+// var h = f*g;
+// alert(h);
+//40
+
+// var f =20;
+// var g="2";
+// var h = f/g;
+// alert(h); 
+// 10
+
+// var  a= 3;
+// var b=5;
+// var c=7;
+
+// var d =b-a*c;
+// alert(d);
+//-16
+
+
+
+// var  a= 3;
+// var b=5;
+// var c=7;
+
+// var d =(b-a)*c;
+// alert(d);
+//14
+
+// var a =5;
+// var b= a++;
+// alert(b)   
+// 5
+
+
+// var a =5;
+// var b= ++a;
+// alert(b)
+// 6
+
+// var x =10;
+// var y = x++ + ++x - x +x++ - ++x + x;
+// alert(y)
+// 22    = 10  + 12 - 12 + 12 - 14 + 14
+//22     = 10      + 0   +12 -14 +  14
+//22     = 10               -2 +14
+//22    = 10  + 12
+//22   =   22
+
+
+//n++  first get premted then get incremented
+//n    the new incrment one get prented
+//++n first get incremented then printed
